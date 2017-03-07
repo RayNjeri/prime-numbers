0b26bd880f149de1a7b4ee91e422741a8b0278a1
Rachael Njeri <njerirachael0@gmail.com>
2017-03-07 12:31:31 +0300

    correction code

--- prime_solution.py
+++ prime_solution.py
@@ -1,18 +1,23 @@
-def prime_num(a):
-	#a=int(raw_input('enter a: '))
-	n = int(2)
-	if a == 0 or a==1:
-		return false
-	elif a<2:
-		return false
-	elif n==2:
-		while n<a:
-			if a % n == 0:
-				return false	
-				break
-				n= n+1
-			else:
-				return true
+def prime_num(n):
+    if n<2:
+        return False
+    else:
+        for i in range(2, n+1):
+            if n % i == 0:
+                return False
+        return True
+print(prime_num)
+
+
+
+def prime_list(a):
+    prime=[]
+    for n in range(2, n):
+        if prime_num (i):
+            prime.append(n)
+    return prime        
+
+
 
 
 
