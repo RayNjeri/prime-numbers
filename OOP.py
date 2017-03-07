f8a2728f0f4b3278880f70e38667c74d9605ed1f
Rachael Njeri <njerirachael0@gmail.com>
2017-03-07 14:57:11 +0300

    class initialisation and attributes

---
+++ OOP.py
@@ -0,0 +1,6 @@
+class animal(object):
+	def __init__(self,sound,number_of_legs,habitation)
+	"""return a new animal object"""
+		self.sound = sound
+		self.number_of_legs = number_of_legs
+		self.habitation = habitation
